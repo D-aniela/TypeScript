eae107984719fc375b5c23d05d674209610ee78b
D-aniela <daniela.es.ib@gmail.com>
2020-02-27 11:06:31 -0600

    Practica para imprimir numeros del 100 al 0 con uso de ciclo y creacion de lista

---
+++ Estructuras_ciclicas/practica6/js/app.js
@@ -0,0 +1,24 @@
+
+
+function eventListener(){
+    let button = document.getElementById('btnInit');
+    button.addEventListener('click', ImprimirNumeros);
+}
+
+eventListener();
+
+function ImprimirNumeros(){
+
+    const listGroup = document.getElementById('Numeros');
+
+    for(let x=100; x>=0; x--){
+        console.log(x);
+
+        const li = document.createElement('li');
+        li.setAttribute('class', 'list-group-item');
+        li.innerText = x;
+
+        listGroup.appendChild(li);
+    }
+}
+    
