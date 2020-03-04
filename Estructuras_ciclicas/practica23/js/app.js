78bb5df176d7bc9c9f6ab6092c724205f46a362b
D-aniela <daniela.es.ib@gmail.com>
2020-03-04 15:19:41 -0600

    practica que suma impares y promedia pares con uso de ciclos

---
+++ Estructuras_ciclicas/practica23/js/app.js
@@ -0,0 +1,34 @@
+/**Elabore un programa que pida al usuario 30 números enteros para que
+al final presente la suma de los números impares y el promedio de los números pares. */
+
+function eventListener() {
+  let button = document.getElementById("btnInit");
+  button.addEventListener("click", sumaPromedio);
+}
+
+eventListener();
+
+function sumaPromedio() {
+  let promediopar = 0;
+  let acumulador = 0;
+  let sumaimpar = 0;
+  let sumapar = 0;
+
+  for (let x = 1; x <= 30; x++) {
+    let numeros = Number(prompt(`Numero ${x}`));
+
+    if (numeros % 2 != 0) {
+      sumaimpar += numeros;
+      console.log(acumulador);
+    }
+
+    if (numeros % 2 != 1) {
+      acumulador++;
+      sumapar += numeros;
+      promediopar = sumapar / acumulador;
+    }
+    const imprimir = document.getElementById("imprimir");
+    imprimir.innerText = `Suma de números impares:  ${sumaimpar}
+    Promedio números pares: ${promediopar}`;
+  }
+}
