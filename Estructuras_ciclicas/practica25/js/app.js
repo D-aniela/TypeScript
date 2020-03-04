9bc8664594ad22af687a1c5baee8537b4f511f87
D-aniela <daniela.es.ib@gmail.com>
2020-03-04 11:03:34 -0600

    Practica que imprime promedio, numeros mayores y positivos con uso de if y for, ciclos

---
+++ Estructuras_ciclicas/practica25/js/app.js
@@ -0,0 +1,38 @@
+/**Elabore una aplicación que lea n números enteros y realice lo siguiente:
+a. Cuantos números leídos fueron mayores que 0.
+b. Promedio de los números positivos.
+c. Promedio de todos los números. */
+
+function eventListener() {
+  let button = document.getElementById("btnInit");
+  button.addEventListener("click", leerNumeros);
+}
+
+eventListener();
+
+function leerNumeros() {
+  let numeros = Number(prompt("Ingrese cantidad de números a evaluar"));
+  let acumulador = 0;
+  let numerospositivos = 0;
+  let promediopositivo = 0;
+  let promedioTotal = 0;
+
+  for (i = 1; i <= numeros; i++) {
+    let calcular = Number(prompt("Ingrese números"));
+
+    acumulador += calcular;
+    promedioTotal = acumulador / numeros;
+
+    if (calcular > 0) {
+      numerospositivos++;
+    }
+    if (calcular > 0) {
+        promediopositivo = acumulador / numerospositivos;
+    }
+
+    const imprimir = document.getElementById("imprimir");
+    imprimir.innerText = `Numeros mayores a 0:  ${numerospositivos}
+    Promedio total: ${promedioTotal}
+    Promedio positivos: ${promediopositivo}`;
+  }
+}
